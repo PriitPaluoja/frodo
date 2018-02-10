fe1c6179a540f0aca6e51a4b9e3c45c33338046b
Kaspar Papli <kaspar.papli@gmail.com>
2018-02-10 15:09:18 +0300

    stop main cluster before starting frodo cluster

--- start_cluster.sh
+++ start_cluster.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
 
-# Starts frodo cluster.
+# 1. Stops main cluster.
+# 2. Starts frodo cluster.
 
-sudo -u postgres /usr/lib/postgresql/9.5/bin/pg_ctl -l /frodo/log.txt -D /frodo start
+version=$(ls /usr/lib/postgresql)
+line_count=$(echo $version | wc -l)
+if [ $line_count -ne 1 ]
+then
+	echo "Found $line_count dirs in /usr/lib/postgresql. Expected 1. Dying slowly..."
+	exit 1
+fi
+
+# stop main cluster
+sudo -u postgres /usr/lib/postgresql/$version/bin/pg_ctl stop -D /etc/postgresql/$version/main 
+
+# start frodo cluster
+sudo -u postgres /usr/lib/postgresql/$version/bin/pg_ctl -l /frodo/log.txt -D /frodo start
 
 echo "Done."
